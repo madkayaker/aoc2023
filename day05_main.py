9241c0e4696d10c45e85e018a339b70f7fc63d0e
madkayaker <paul@madkayaker.co.uk>
2023-12-09 21:13:40 +0000

    Adding day05 files

---
+++ day05_main.py
@@ -0,0 +1,22 @@
+def main():
+    input_text = open('input/day05_test.txt').read()
+    # input_text = open('./input/day05.txt', 'r').read()
+    print(f'Part One: {part_one(input_text)}')
+    print(f'Part Two: {part_two(input_text)}')
+
+
+def part_one(input_text):
+    total = 0
+    lines = input_text.splitlines()
+
+
+
+    return total
+
+
+def part_two(input_text):
+
+    return
+
+if __name__ == '__main__':
+    raise SystemExit(main())
