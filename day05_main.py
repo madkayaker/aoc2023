35d3b32818ec021ce2a21c4821750d13a8a8079a
madkayaker <paul@madkayaker.co.uk>
2023-12-16 22:58:34 +0000

    day05 part 1 completed, a bit shoddy, but it works

--- day05_main.py
+++ day05_main.py
@@ -6,17 +6,43 @@ def main():
 
 
 def part_one(input_text):
-    total = 0
-    lines = input_text.splitlines()
+    seed_map = {}
+    seeds = input_text.split('\n\n')[0].replace('seeds: ', '').split(' ')
+    seeds = [eval(s) for s in seeds]
 
+    for seed in seeds:
+        print(seed)
+        number = seed
+        q = number
 
+        maps = input_text.split('\n\n')[1:]
+        print(maps)
+        for x in maps:
+            map = x.split('\n')[1:]
+            print('map ', map)
+            for row in map:
+                print(f'row {row}')
+                test = row.split(' ')
+                d, s, i = [eval(x) for x in test]
+                print(f'd {d}, s {s}, i {i}, number {number}, q {q}')
+                if s <= number and number < (s + i):
+                    print('got a thing')
+                    q = number - s + d
+                else:
+                    print('not got a thing')
+                    continue
 
-    return total
+                print(f'{x.split(":")[0]}: {q}')
+            number = q
+        seed_map[seed] = q
+
+    return min(seed_map.values())
 
 
 def part_two(input_text):
 
     return
+
 
 if __name__ == '__main__':
     raise SystemExit(main())
